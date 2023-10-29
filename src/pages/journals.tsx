0b87ef4d066658613af74aaf495a86c938525cf3
apolyeti <azhandarveen@gmail.com>
2023-10-29 00:18:13 -0700

    add openai journal analysis

--- src/pages/journals.tsx
+++ src/pages/journals.tsx
@@ -1,17 +1,18 @@
 import { NextPage } from "next";
 import NextLink from "next/link"
-import { Center, Text, Heading, VStack, Input, Button, IconButton, useDisclosure } from "@chakra-ui/react"
+import { Center, Text, Box, VStack, Input, Button, IconButton, HStack } from "@chakra-ui/react"
 import { ChatIcon } from "@chakra-ui/icons"
 import Loading from "@components/Loading"
 import { useState, useRef, useEffect } from "react"
 import {Journal} from "@types"
 import JournalEntry from "@components/JournalEntry";
-import { getJournal } from "@utils/api";
+import { analyzeJournals, getJournal } from "@utils/api";
 
 export default function Journal() : NextPage {
+    const [summary, setSummary] = useState<string>("");
+    const [events, setEvents] = useState<string>("");
     const [journals, setJournals] = useState<Journal[]>([]);
     useEffect(() => {
-        console.log('asdasd');
         const getJournals = async () => {
             const req = await getJournal({username: 'Waylon'});
             setJournals(req);
@@ -26,6 +27,20 @@ export default function Journal() : NextPage {
 
     }
     , []);
+    const handleAI = async () => {
+        try {
+            const req = await analyzeJournals();
+            if (req) {
+                console.log(req);
+                setSummary(req.summary);
+                setEvents(req.events);
+            } else {
+                console.error('Error fetching journals:', req);
+            }
+        } catch (error) {
+            console.error('Error fetching journals:', error);
+        }
+    }
     // make div space the journals from the top of the screen
     return (
         <VStack display={"flex"}>
@@ -50,10 +65,49 @@ export default function Journal() : NextPage {
                 Back
             </Button>
         </NextLink>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-floppy" viewBox="0 0 16 16">
-            <path d="M11 2H9v3h2V2Z"/>
-            <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0ZM1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5Zm3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4v4.5ZM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5V15Z"/>
-        </svg>
+        <Button 
+            bgColor={"transparent"}
+            _hover={{
+                bgColor: "transparent",
+                transform: "scale(1.09); duration: 0.5s",
+                color: "gray.300"
+            }}
+            _active={{
+                bgColor: "transparent",
+                transform: "scale(1.09); duration: 0.5s",
+                color: "gray.300"
+            }}
+            onClick={handleAI}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                width="16" 
+                height="16" 
+                fill="currentColor" 
+                class="bi bi-floppy" 
+                viewBox="0 0 16 16"
+            >
+                <path d="M11 2H9v3h2V2Z"/>
+                <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0ZM1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5Zm3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4v4.5ZM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5V15Z"/>
+            </svg>
+        </Button>
+        {events && summary &&
+        <HStack
+            bgColor={"#330F06"}
+            borderRadius={"lg"}
+            padding={"2vh"}
+            borderWidth={"1px"}
+        >
+            <Box>
+            <Text>
+                {summary}
+            </Text>
+            </Box>
+            <Box>
+                <Text>
+                    {events}
+                </Text>
+            </Box>
+        </HStack>}
         </VStack>
     )
 }
